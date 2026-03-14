ed9cb4fe0f30a088b63361a9cf76fb14e35fb7ff
Omswarup Nanda <nandaomswarup@example.com>
2026-03-14 12:11:36 +0530

    Complete DevOps CI/CD pipeline with Jenkins, Docker, Kubernetes, Terraform

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -1,11 +1,10 @@
-# SERVER1: 'MASTER-SERVER' (with Jenkins, Maven, Docker, Ansible, Trivy)
-# STEP1: CREATING A SECURITY GROUP FOR JENKINS SERVER
-# Description: Allow SSH, HTTP, HTTPS, 8080, 8081
+# SERVER1: 'MASTER-SERVER' (Jenkins, Maven, Docker, Ansible, Trivy)
+
+# STEP1: SECURITY GROUP FOR JENKINS SERVER
 resource "aws_security_group" "my_security_group1" {
   name        = "my-security-group1"
-  description = "Allow SSH, HTTP, HTTPS, 8080 for Jenkins & Maven"
+  description = "Allow SSH, HTTP, HTTPS, Jenkins"
 
-  # SSH Inbound Rules
   ingress {
     from_port   = 22
     to_port     = 22
@@ -41,7 +40,6 @@ resource "aws_security_group" "my_security_group1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # SSH Outbound Rules
   egress {
     from_port   = 0
     to_port     = 0
@@ -50,19 +48,17 @@ resource "aws_security_group" "my_security_group1" {
   }
 }
 
-# STEP2: CREATE AN JENKINS EC2 INSTANCE USING EXISTING PEM KEY
-# Note: i. First create a pem-key manually from the AWS console
-#      ii. Copy it in the same directory as your terraform code
+# STEP2: CREATE JENKINS EC2 INSTANCE
 resource "aws_instance" "my_ec2_instance1" {
-  ami                    = "ami-0cf10cdf9fcd62d37"
-  instance_type          = "t2.medium"
-  vpc_security_group_ids = [aws_security_group.my_security_group1.id]
-  key_name               = "My_Key" # paste your key-name here, do not use extension '.pem'
 
-  # Consider EBS volume 30GB
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "c7i-flex.large"
+  vpc_security_group_ids = [aws_security_group.my_security_group1.id]
+  key_name               = "My_Key"
+
   root_block_device {
-    volume_size = 30    # Volume size 30 GB
-    volume_type = "gp2" # General Purpose SSD
+    volume_size = 30
+    volume_type = "gp3"
   }
 
   tags = {
@@ -70,64 +66,51 @@ resource "aws_instance" "my_ec2_instance1" {
   }
 
   user_data = <<-EOF
-    #!/bin/bash
-    # wait for 1min before EC2 initialization
-    sleep 60
-    sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-    sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-    sudo yum install -y apache-maven
-    sudo yum install java-1.8.0-devel -y
-  EOF
+#!/bin/bash
+sleep 60
+yum update -y
+yum install -y git java-17-amazon-corretto maven
+EOF
 
-  # STEP3: USING REMOTE-EXEC PROVISIONER TO INSTALL TOOLS
   provisioner "remote-exec" {
-    # ESTABLISHING SSH CONNECTION WITH EC2
+
     connection {
       type        = "ssh"
-      private_key = file("./My_Key.pem") # replace with your key-name 
+      private_key = file("./My_Key.pem")
       user        = "ec2-user"
       host        = self.public_ip
     }
 
-    inline = [        
-      # wait for 200sec before EC2 initialization
-      "sleep 200",
-      # Install Git 
-      "sudo yum install git -y",
-      
-      # Install Jenkins 
-      # REF: https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/
+    inline = [
+
+      "sleep 300",
+
+      # Install Jenkins
       "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
       "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
-      "sudo yum install java-17-amazon-corretto -y",
       "sudo yum install jenkins -y",
       "sudo systemctl enable jenkins",
       "sudo systemctl start jenkins",
 
       # Install Docker
-      # REF: https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-docker.html
-      "sudo yum update -y",
       "sudo yum install docker -y",
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
+      "sudo usermod -aG docker ec2-user",
       "sudo usermod -aG docker jenkins",
-      # To avoid below permission error
-      # Got permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock
-      "sudo chmod 666 /var/run/docker.sock",
 
       # Install Trivy
-      # REF: https://aquasecurity.github.io/trivy/v0.18.3/installation/
-      "sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm",
+      "sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/latest/download/trivy_0.50.0_Linux-64bit.rpm",
 
       # Install Ansible
-      "sudo yum update -y",
-      "sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y",
-      "sudo yum install git python python-devel python-pip openssl ansible -y",
+      "sudo yum install python3-pip -y",
+      "pip3 install ansible"
     ]
   }
 }
 
-# STEP3: OUTPUT PUBLIC IP OF EC2 INSTANCE
+# OUTPUTS
+
 output "ACCESS_YOUR_JENKINS_HERE" {
   value = "http://${aws_instance.my_ec2_instance1.public_ip}:8080"
 }
@@ -136,12 +119,10 @@ output "Jenkins_Initial_Password" {
   value = "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
 }
 
-# STEP4: OUTPUT PUBLIC IP OF EC2 INSTANCE
 output "MASTER_SERVER_PUBLIC_IP" {
   value = aws_instance.my_ec2_instance1.public_ip
 }
 
-# STEP5: OUTPUT PRIVATE IP OF EC2 INSTANCE
 output "MASTER_SERVER_PRIVATE_IP" {
   value = aws_instance.my_ec2_instance1.private_ip
 }
--- terraform_files/node.tf
+++ terraform_files/node.tf
@@ -1,149 +1,155 @@
-# SERVER2: 'NODE-SERVER' (with Docker & Kubernetes)
-# STEP1: CREATING A SECURITY GROUP FOR DOCKER-K8S
-# Description: K8s requires ports 22, 80, 443, 6443, 8001, 10250, 30000-32767
+# SERVER2: 'NODE-SERVER' (Docker + Kubernetes)
+
+# STEP1: SECURITY GROUP FOR K8S NODE
 resource "aws_security_group" "my_security_group2" {
-  name        = "my-security-group4"
-  description = "Allow K8s ports"
+  name        = "my-security-group2"
+  description = "Allow Kubernetes ports"
 
-  # SSH Inbound Rules
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
+    from_port = 6443
+    to_port   = 6443
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 8001
-    to_port     = 8001
-    protocol    = "tcp"
+    from_port = 8001
+    to_port   = 8001
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
+    from_port = 10250
+    to_port   = 10250
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
+    from_port = 30000
+    to_port   = 32767
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # SSH Outbound Rules
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# STEP2: CREATE A K8S EC2 INSTANCE USING EXISTING PEM KEY
-# Note: i. First create a pem-key manually from the AWS console
-#      ii. Copy it in the same directory as your terraform code
+# STEP2: CREATE K8S EC2 INSTANCE
 resource "aws_instance" "my_ec2_instance2" {
-  ami                    = "ami-0cf10cdf9fcd62d37"
-  instance_type          = "t2.medium" # K8s requires min 2CPU & 4G RAM
-  vpc_security_group_ids = [aws_security_group.my_security_group2.id]
-  key_name               = "My_Key" # paste your key-name here, do not use extension '.pem'
 
-  # Consider EBS volume 30GB
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "c7i-flex.large"
+
+  vpc_security_group_ids = [aws_security_group.my_security_group2.id]
+  key_name               = "My_Key"
+
   root_block_device {
-    volume_size = 30    # Volume size 30 GB
-    volume_type = "gp2" # General Purpose SSD
+    volume_size = 30
+    volume_type = "gp3"
   }
 
   tags = {
     Name = "NODE-SERVER"
   }
 
-  # STEP3: USING REMOTE-EXEC PROVISIONER TO INSTALL TOOLS
   provisioner "remote-exec" {
-    # ESTABLISHING SSH CONNECTION WITH EC2
+
     connection {
       type        = "ssh"
-      private_key = file("./My_Key.pem") # replace with your key-name 
+      private_key = file("./My_Key.pem")
       user        = "ec2-user"
       host        = self.public_ip
     }
 
-      inline = [
-      "sleep 200",
+    inline = [
+
+      "sleep 300",
+
+      # Update system
+      "sudo yum update -y",
 
       # Install Docker
-      # REF: https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
-      "sudo yum update -y",
       "sudo yum install docker -y",
-      "sudo systemctl start docker",
       "sudo systemctl enable docker",
-      "sudo chmod 777 /var/run/docker.sock",
-      
-      # Install K8s
-      # REF: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+      "sudo systemctl start docker",
+      "sudo usermod -aG docker ec2-user",
+
+      # Disable SELinux
       "sudo setenforce 0",
-      "sudo sed -i 's/^SELINUX=enforcing$$/SELINUX=permissive/' /etc/selinux/config",
-      "cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo",
-      "[kubernetes]",
-      "name=Kubernetes",
-      "baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/",
-      "enabled=1",
-      "gpgcheck=1",
-      "gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key",
-      "exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni",
-      "EOF",
+      "sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config",
+
+      # Kubernetes repo
+      "echo '[kubernetes]' | sudo tee /etc/yum.repos.d/kubernetes.repo",
+      "echo 'name=Kubernetes' | sudo tee -a /etc/yum.repos.d/kubernetes.repo",
+      "echo 'baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/' | sudo tee -a /etc/yum.repos.d/kubernetes.repo",
+      "echo 'enabled=1' | sudo tee -a /etc/yum.repos.d/kubernetes.repo",
+      "echo 'gpgcheck=1' | sudo tee -a /etc/yum.repos.d/kubernetes.repo",
+      "echo 'gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key' | sudo tee -a /etc/yum.repos.d/kubernetes.repo",
+      "echo 'exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni' | sudo tee -a /etc/yum.repos.d/kubernetes.repo",
+
+      # Install Kubernetes
       "sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes",
-      "sudo systemctl enable --now kubelet",
-      "sudo kubeadm init --pod-network-cidr=10.244.0.0/16  --ignore-preflight-errors=NumCPU --ignore-preflight-errors=Mem",
-      "sudo mkdir -p $HOME/.kube",
+      "sudo systemctl enable kubelet",
+      "sudo systemctl start kubelet",
+
+      # Initialize cluster
+      "sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --ignore-preflight-errors=NumCPU --ignore-preflight-errors=Mem",
+
+      # Configure kubectl
+      "mkdir -p $HOME/.kube",
       "sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config",
       "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
-      "kubectl apply -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml",
-      "kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml",
-      "kubectl taint nodes --all node-role.kubernetes.io/control-plane-",
-      ]
-    }
-  
+
+      # Install Calico
+      "kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml",
+
+      # Allow pods on control-plane
+      "kubectl taint nodes --all node-role.kubernetes.io/control-plane-"
+    ]
+  }
 }
 
-# STEP3: OUTPUT PUBLIC IP OF EC2 INSTANCE
+# OUTPUTS
+
 output "NODE_SERVER_PUBLIC_IP" {
   value = aws_instance.my_ec2_instance2.public_ip
 }
 
-# STEP4: OUTPUT PRIVATE IP OF EC2 INSTANCE
 output "NODE_SERVER_PRIVATE_IP" {
   value = aws_instance.my_ec2_instance2.private_ip
 }
